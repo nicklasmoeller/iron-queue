e3ed0210e0bf1152d9137a0a5eec1b6ad10fe8b5
nicklasmoeller <hello@nicklasmoeller.com>
2016-12-23 17:03:55 +0100

    Laravel 5.3 support

--- src/IronQueue.php
+++ src/IronQueue.php
@@ -65,6 +65,18 @@ class IronQueue extends Queue implements QueueContract
     }
 
     /**
+     * Get the size of the queue.
+     *
+     * @param  string  $queue
+     * @return int
+     */
+    public function size($queue = null)
+    {
+        $queue = $this->getQueue($queue);
+        return (int) $this->iron->getQueue($queue)->size;
+    }
+
+    /**
      * Push a new job onto the queue.
      *
      * @param string $job
